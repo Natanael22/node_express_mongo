41ef996cc7e27bce0cf17a4cc013fb03470b60b4
Natanael22 <natanaelcorreia1@gmail.com>
2024-01-05 18:43:52 -0300

    incluido moongose e feito testes de integracao

--- src/app.js
+++ src/app.js
@@ -1,53 +1,50 @@
 import express from "express";
+import connectDB from "./config/dbConnect.js";
+import livro from "./models/Livro.js";
+
+const conexao = await connectDB();
+
+conexao.on("error", (erro) =>{
+    console.error("erro de conexao: ", erro);
+})
+
+conexao.once("open", () =>{
+    console.log("conexao bem sucedida");
+})
 
 const app = express();
 app.use(express.json());
 
-const livros = [
-    {
-        id:1,
-        nome:"livro de programacao"
-    },
-    {
-        id:2,
-        nome:"livro de culinaria"
-    }
-]
-
-function buscaLivro(id){
-    return livros.findIndex(livros => {
-        return livros.id === Number(id);
-    })
-}
-
-app.get("/", (req, res) =>{
+app.get("/", (req, res) =>{    
     res.status(200).send("Curso Node JS com Express");
 });
 
-app.get("/livros", (req, res) => {
-    res.status(200).json(livros);
+app.get("/livros", async (req, res) => {
+    const listaLivros = await livro.find({});
+    res.status(200).json(listaLivros);
 });
 
-app.get("/livros/:id", (req, res) =>{
-    const index = buscaLivro(req.params.id);
-    res.status(200).json(livros[index]);
+app.get("/livros/:id", async (req, res) =>{
+    const book = await livro.findById(req.params.id);
+    res.status(200).json(book);
 });
 
 app.post("/livros", (req, res) => {
-    livros.push(req.body);
+    livro.create(req.body);    
     res.status(201).json({mensagem : "livro criado"});
     
 });
 
-app.put("/livros/:id", (req, res) =>{
-    const index = buscaLivro(req.params.id);
-    livros[index].nome = req.body.nome;
-    res.status(200).json(livros[index]);
+app.put("/livros/:id", async (req, res) =>{
+    const book = await livro.findById(req.params.id);
+    book.titulo = req.body.titulo;
+    await livro.updateOne(book);
+    res.status(200).json(book);
 });
 
-app.delete("/livros/:id", (req, res) =>{
-    const index = buscaLivro(req.params.id);
-    livros.splice(index, 1);
+app.delete("/livros/:id", async (req, res) =>{
+    const book = await livro.findById(req.params.id);
+    await livro.deleteOne(book);
     res.status(200).json({mensagem : "livro removido"});
 });
 
