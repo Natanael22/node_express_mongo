6d56d77d83633381b5613eb0d3bbc12a64290674
Natanael22 <natanaelcorreia1@gmail.com>
2024-02-02 13:45:36 -0300

    refactor referencias

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 //import http from "http";
-import "dotenv/config"
+import "dotenv/config";
 import app from "./src/app.js";
 
 const PORT = 3000;
 
 app.listen(PORT, () => {
+  // eslint-disable-next-line indent
     console.log("server running!");
 });
--- src/routes/livrosRoutes.js
+++ src/routes/livrosRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import LivroController from "../controllers/LivroController.js";
+import LivroController from "../controllers/livroController.js";
 import paginar from "../middlewares/paginar.js";
 
 const routes = express.Router();
